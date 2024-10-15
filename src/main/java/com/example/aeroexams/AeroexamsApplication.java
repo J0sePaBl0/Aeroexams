4820413d3073f91e30a088ab413ad0644c6bcb27
unknown <artaviajose2103@gmail.com>
2024-10-15 10:57:34 -0600

    initial commit

---
+++ src/main/java/com/example/aeroexams/AeroexamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.aeroexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroexamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroexamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aeroexams/AeroexamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aeroexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroexamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
